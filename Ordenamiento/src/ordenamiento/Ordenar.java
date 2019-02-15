88bfd5c8c7b45dd8f19d2ea378a2654a9e6fe246
MariaCaicedo <camila19970426@gmail.com>
2019-02-15 12:27:55 -0500

    shellshort
    se agregan mas metodos de ordenameinto, se agrega toda la logica de shellshort

--- Ordenamiento/src/ordenamiento/Ordenar.java
+++ Ordenamiento/src/ordenamiento/Ordenar.java
@@ -2,10 +2,33 @@ package ordenamiento;
 
 public class Ordenar {
     public static int[] shellsort(int[] vector) {
+        int size = vector.length;
+        for (int grap = size / 2; grap > 0; grap /= 2) {
+            for (int i = grap; i < size; i++) {
+                int temp = vector[i];
+                int j;
+                for (j = i; j >= grap && vector[j - grap] > temp; j -= grap) {
+                    vector[j] = vector[j - grap];
+                }
+                vector[j] = temp;
+            }
+        }
         return vector;
     }
 
     public static int[] quicksort(int[] vector) {
+        return vector;
+    }
+
+    public static int[] radixSort(int[] vector) {
+        return vector;
+    }
+
+    public static int[] bucketSort(int[] vector) {
+        return vector;
+    }
+
+    public static int[] mergeShort(int[] vector) {
         return vector;
     }
 }
