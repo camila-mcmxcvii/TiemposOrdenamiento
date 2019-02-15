2a2d89dc2f371a5538107feb9b0548cce900db5f
MariaCaicedo <camila19970426@gmail.com>
2019-02-15 14:11:08 -0500

    Update Ordenar.java
    se agrega quicksort y mergesort

--- Ordenamiento/src/ordenamiento/Ordenar.java
+++ Ordenamiento/src/ordenamiento/Ordenar.java
@@ -16,7 +16,27 @@ public class Ordenar {
         return vector;
     }
 
-    public static int[] quicksort(int[] vector) {
+    public static int[] quicksort(int[] vector, int izq, int der) {
+        int pivote = vector[izq];
+        int izquierdo = izq;
+        int derecho = der;
+        int aux;
+
+        while (izquierdo < derecho) {
+            while (vector[izquierdo] <= pivote && izquierdo < derecho) izquierdo++;
+            while (vector[derecho] > pivote) derecho--;
+            if (izquierdo < derecho) {
+                aux = vector[izquierdo];
+                vector[izquierdo] = vector[derecho];
+                vector[derecho] = aux;
+            }
+        }
+        vector[izq] = vector[derecho];
+        vector[derecho] = pivote;
+        if (izq < derecho - 1)
+            quicksort(vector, izq, derecho - 1);
+        if (derecho + 1 < der)
+            quicksort(vector, derecho + 1, der);
         return vector;
     }
 
@@ -29,6 +49,52 @@ public class Ordenar {
     }
 
     public static int[] mergeShort(int[] vector) {
+        sort(vector, 0, vector.length - 1);
         return vector;
+    }
+
+    private static void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static void sort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
     }
 }
